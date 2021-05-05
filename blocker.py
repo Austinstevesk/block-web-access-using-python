4b406190e52feb5acce9e87a32b3b346cc542f64
Austinstevesk <austinstevesk@gmail.com>
2021-05-05 18:44:00 +0300

    Initial commit

--- blocker.py
+++ blocker.py
@@ -1,5 +1,5 @@
 import time
-from datetime import datetime
+from datetime import datetime as dt
 
 #Windows host file path
 hostpath = r"C:\Windows\System32\drivers\etc\hosts"
@@ -9,4 +9,8 @@ redirect = "127.0.0.1"
 websites = ["www.youtube.com", "youtube.com", "web.whatsapp.com"]
 
 while True:
-    
+    #Duration to block the websites
+    if dt(dt.now().year, dt.now().month, dt.now().day, 9) < dt.now() < dt(dt.now(). year, dt.now().month, dt.now().day, 18):
+        print("Sorry Not Allowed")
+        with open(hostpath, 'r+') as file:
+            content = file.read()
