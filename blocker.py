6c083bac4fd67cd595c6d8fed80ed93b2bcb62c0
Austinstevesk <austinstevesk@gmail.com>
2021-05-05 18:39:15 +0300

    Initial commit

--- blocker.py
+++ blocker.py
@@ -5,3 +5,8 @@ from datetime import datetime
 hostpath = r"C:\Windows\System32\drivers\etc\hosts"
 redirect = "127.0.0.1"
 
+#websites to block
+websites = ["www.youtube.com", "youtube.com", "web.whatsapp.com"]
+
+while True:
+    
